4e5eef94ad4984a1f3630ffcb1ccaa5dc0d979d9
Johan Larsson <johan_gavle1@hotmail.com>
2016-08-22 19:47:46 +0200

    wip

---
+++ Gu.Reactive.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Gu.Reactive.Benchmarks
+{
+    using BenchmarkDotNet.Running;
+
+    public class Program
+    {
+        public static void Main(string[] name)
+        {
+            BenchmarkRunner.Run<ObservePropertyChanged>();
+        }
+    }
+}
